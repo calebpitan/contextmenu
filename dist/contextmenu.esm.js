6280ee9e06df63394d8a15dd23628e5f5c8ac1ac
CalebPitan <36812893+CalebPitan@users.noreply.github.com>
2019-09-29 01:45:31 +0100

    dist

--- dist/contextmenu.esm.js
+++ dist/contextmenu.esm.js
@@ -1,3 +1,24 @@
+var Device = {
+    isMobile: false,
+    isDesktop: false
+};
+Object.defineProperty(Device, 'isMobile', {
+    get: function () {
+        var mobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|Windows Phone/i;
+        if (navigator.userAgent.match(mobile)) {
+            return true;
+        }
+        return false;
+    },
+    enumerable: true,
+    configurable: true
+});
+Object.defineProperty(Device, 'isDesktop', {
+    get: function () { return !Device.isMobile; },
+    enumerable: true,
+    configurable: true
+});
+
 function findPath(target, path) {
     if (path === void 0) { path = [target]; }
     var Target = target;
@@ -8,8 +29,12 @@ function findPath(target, path) {
 
 function clickAway(src, handler, away) {
     if (away === void 0) { away = document; }
-    var event = 'click';
+    var event = Device.isMobile ? 'touchstart' : 'mousedown';
     var listener = function (mouseEvent) {
+        // @ts-ignore
+        if (mouseEvent.type === 'mousedown' && mouseEvent.buttons !== 1) {
+            return;
+        }
         // @ts-ignore
         var path = mouseEvent.path ||
             (mouseEvent.composedPath && mouseEvent.composedPath()) || findPath(mouseEvent.target);
@@ -18,16 +43,24 @@ function clickAway(src, handler, away) {
         }
     };
     return {
-        flip: function () { return away.addEventListener(event, listener); },
-        flap: function () { return away.removeEventListener(event, listener); }
+        flip: function () { return away.addEventListener(event, listener, true); },
+        flap: function () { return away.removeEventListener(event, listener, true); }
     };
 }
+
+var Path = /** @class */ (function () {
+    function Path(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    return Path;
+}());
 
 var Position = /** @class */ (function () {
     function Position(box, elementBox) {
         this.box = box;
         this.elementBox = elementBox;
-        this.positioning = 'absolute';
+        this.positioning = 'relative';
     }
     Object.defineProperty(Position.prototype, "type", {
         set: function (position) {
@@ -40,10 +73,7 @@ var Position = /** @class */ (function () {
         var x0 = _a.x, y0 = _a.y;
         var x = this.generateX(x0);
         var y = this.generateY(y0);
-        return {
-            x: x,
-            y: y
-        };
+        return new Path(x, y);
     };
     Position.prototype.viewportExcess = function (_a) {
         var x = _a.x, y = _a.y;
@@ -53,26 +83,40 @@ var Position = /** @class */ (function () {
             right: false,
             bottom: false
         };
-        vpq.right = this.elementBox.width + x > this.box.width;
-        vpq.bottom = this.elementBox.height + y > this.box.height;
+        if (this.positioning !== 'fixed') {
+            vpq.right = this.elementBox.width + x > this.box.width;
+            vpq.bottom = this.elementBox.height + y > this.box.height;
+        }
         return vpq;
     };
     Position.prototype.generateX = function (x) {
-        if (this.positioning === 'relative') {
+        if (this.positioning === 'absolute') {
             return this.elementBox.width + x > this.box.width ? x - this.elementBox.width : x;
         }
-        return this.getAbsoluteX(x);
+        else if (this.positioning === 'static') {
+            return x;
+        }
+        else if (this.positioning === 'fixed') {
+            return (this.box.width - this.elementBox.width) / 2;
+        }
+        return this.getRelativeX(x);
     };
     Position.prototype.generateY = function (y) {
-        if (this.positioning === 'relative') {
+        if (this.positioning === 'absolute') {
             return this.elementBox.height + y > this.box.height ? y - this.elementBox.height : y;
         }
-        return this.getAbsoluteY(y);
+        else if (this.positioning === 'static') {
+            return y;
+        }
+        else if (this.positioning === 'fixed') {
+            return (this.box.height - this.elementBox.height) / 2;
+        }
+        return this.getRelativeY(y);
     };
-    Position.prototype.getAbsoluteX = function (x) {
+    Position.prototype.getRelativeX = function (x) {
         return this.elementBox.width + x > this.box.width ? this.box.width - x : x;
     };
-    Position.prototype.getAbsoluteY = function (y) {
+    Position.prototype.getRelativeY = function (y) {
         return this.elementBox.height + y > this.box.height ? this.box.height - y : y;
     };
     return Position;
@@ -102,35 +146,6 @@ var Box2D = /** @class */ (function () {
     return Box2D;
 }());
 
-var Device = {
-    isMobile: false,
-    isDesktop: false
-};
-Object.defineProperty(Device, 'isMobile', {
-    get: function () {
-        var mobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|Windows Phone/i;
-        if (navigator.userAgent.match(mobile)) {
-            return true;
-        }
-        return false;
-    },
-    enumerable: true,
-    configurable: true
-});
-Object.defineProperty(Device, 'isDesktop', {
-    get: function () { return !Device.isMobile; },
-    enumerable: true,
-    configurable: true
-});
-
-var Path = /** @class */ (function () {
-    function Path(x, y) {
-        this.x = x;
-        this.y = y;
-    }
-    return Path;
-}());
-
 var VALIDATE = 'validate';
 var FAILED = 'failed';
 var CLICK_AWAY = 'clickaway';
@@ -141,19 +156,31 @@ var Events = {
     CTXMENU: 'contextmenu'
 };
 var EVENT_OPT = true;
+var NAME = 'ContextMenu';
+// @ts-ignore
+var DUMP = window[NAME];
 var ContextMenu = /** @class */ (function () {
     function ContextMenu(src, dest, windowSize) {
         this.src = src;
         this.dest = dest;
         this.timeout = 800;
-        this.positioning = 'absolute';
+        this.positioning = 'relative';
         this.handlers = {};
         var box = new Box2D(0, 0, windowSize.width, windowSize.height);
         var box2 = new Box2D(0, 0, this.dest.offsetWidth, this.dest.offsetHeight);
         this.ps = new Position(box, box2);
     }
+    // tslint:disable-next-line:member-ordering
+    ContextMenu.namespace = function (name) {
+        // @ts-ignore
+        window[name] = window[name] || {};
+        // @ts-ignore
+        window[name][NAME] = ContextMenu;
+        // @ts-ignore
+        window[NAME] = DUMP;
+    };
     ContextMenu.prototype.setup = function (setup) {
-        if (setup === void 0) { setup = { timeout: 800, position: 'absolute' }; }
+        if (setup === void 0) { setup = { timeout: 800, position: 'relative' }; }
         this.timeout = setup.timeout;
         this.positioning = setup.position;
         this.ps.type = this.positioning;
@@ -168,8 +195,8 @@ var ContextMenu = /** @class */ (function () {
             touchMoved = true;
         };
         var end = function () {
-            _this.src.removeEventListener(Events.TOUCHMOVE, move, true);
-            _this.src.removeEventListener(Events.TOUCHEND, end, true);
+            _this.src.removeEventListener(Events.TOUCHMOVE, move, EVENT_OPT);
+            _this.src.removeEventListener(Events.TOUCHEND, end, EVENT_OPT);
             clearTimeout(id);
         };
         var touchMoved = false;
@@ -181,10 +208,10 @@ var ContextMenu = /** @class */ (function () {
             _this.src.addEventListener(Events.TOUCHMOVE, move, EVENT_OPT);
             _this.src.addEventListener(Events.TOUCHEND, end, EVENT_OPT);
             var signal = new Path(touchEvent.touches[0].clientX, touchEvent.touches[0].clientY);
-            var response = _this.ps.from(signal);
+            var coords = _this.ps.from(signal);
             id = setTimeout(function () {
                 if (!touchMoved && typeof _this.handlers[VALIDATE] === 'function') {
-                    _this.handlers[VALIDATE].call(_this, new Path(response.x, response.y), _this.ps.viewportExcess(signal));
+                    _this.handlers[VALIDATE].call(_this, coords, _this.ps.viewportExcess(signal));
                 }
                 else if (touchMoved && typeof _this.handlers[FAILED] === 'function') {
                     _this.handlers[FAILED].call(_this);
@@ -196,9 +223,9 @@ var ContextMenu = /** @class */ (function () {
             ctxEvent.preventDefault();
             var signal = new Path(ctxEvent.clientX, ctxEvent.clientY);
             var response = _this.ps.from(signal);
-            _this.handlers[VALIDATE].call(_this, new Path(response.x, response.y), _this.ps.viewportExcess(signal));
+            _this.handlers[VALIDATE].call(_this, response, _this.ps.viewportExcess(signal));
         };
-        if (Device.isMobile || navigator.maxTouchPoints || navigator.msMaxTouchPoints) {
+        if (Device.isMobile && (navigator.maxTouchPoints || navigator.msMaxTouchPoints)) {
             this.src.addEventListener(Events.TOUCHSTART, this.start, EVENT_OPT);
         }
         else {
--- dist/contextmenu.js
+++ dist/contextmenu.js
@@ -4,6 +4,27 @@
   (global = global || self, global.ContextMenu = factory());
 }(this, function () { 'use strict';
 
+  var Device = {
+      isMobile: false,
+      isDesktop: false
+  };
+  Object.defineProperty(Device, 'isMobile', {
+      get: function () {
+          var mobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|Windows Phone/i;
+          if (navigator.userAgent.match(mobile)) {
+              return true;
+          }
+          return false;
+      },
+      enumerable: true,
+      configurable: true
+  });
+  Object.defineProperty(Device, 'isDesktop', {
+      get: function () { return !Device.isMobile; },
+      enumerable: true,
+      configurable: true
+  });
+
   function findPath(target, path) {
       if (path === void 0) { path = [target]; }
       var Target = target;
@@ -14,8 +35,12 @@
 
   function clickAway(src, handler, away) {
       if (away === void 0) { away = document; }
-      var event = 'click';
+      var event = Device.isMobile ? 'touchstart' : 'mousedown';
       var listener = function (mouseEvent) {
+          // @ts-ignore
+          if (mouseEvent.type === 'mousedown' && mouseEvent.buttons !== 1) {
+              return;
+          }
           // @ts-ignore
           var path = mouseEvent.path ||
               (mouseEvent.composedPath && mouseEvent.composedPath()) || findPath(mouseEvent.target);
@@ -24,16 +49,24 @@
           }
       };
       return {
-          flip: function () { return away.addEventListener(event, listener); },
-          flap: function () { return away.removeEventListener(event, listener); }
+          flip: function () { return away.addEventListener(event, listener, true); },
+          flap: function () { return away.removeEventListener(event, listener, true); }
       };
   }
+
+  var Path = /** @class */ (function () {
+      function Path(x, y) {
+          this.x = x;
+          this.y = y;
+      }
+      return Path;
+  }());
 
   var Position = /** @class */ (function () {
       function Position(box, elementBox) {
           this.box = box;
           this.elementBox = elementBox;
-          this.positioning = 'absolute';
+          this.positioning = 'relative';
       }
       Object.defineProperty(Position.prototype, "type", {
           set: function (position) {
@@ -46,10 +79,7 @@
           var x0 = _a.x, y0 = _a.y;
           var x = this.generateX(x0);
           var y = this.generateY(y0);
-          return {
-              x: x,
-              y: y
-          };
+          return new Path(x, y);
       };
       Position.prototype.viewportExcess = function (_a) {
           var x = _a.x, y = _a.y;
@@ -59,26 +89,40 @@
               right: false,
               bottom: false
           };
-          vpq.right = this.elementBox.width + x > this.box.width;
-          vpq.bottom = this.elementBox.height + y > this.box.height;
+          if (this.positioning !== 'fixed') {
+              vpq.right = this.elementBox.width + x > this.box.width;
+              vpq.bottom = this.elementBox.height + y > this.box.height;
+          }
           return vpq;
       };
       Position.prototype.generateX = function (x) {
-          if (this.positioning === 'relative') {
+          if (this.positioning === 'absolute') {
               return this.elementBox.width + x > this.box.width ? x - this.elementBox.width : x;
           }
-          return this.getAbsoluteX(x);
+          else if (this.positioning === 'static') {
+              return x;
+          }
+          else if (this.positioning === 'fixed') {
+              return (this.box.width - this.elementBox.width) / 2;
+          }
+          return this.getRelativeX(x);
       };
       Position.prototype.generateY = function (y) {
-          if (this.positioning === 'relative') {
+          if (this.positioning === 'absolute') {
               return this.elementBox.height + y > this.box.height ? y - this.elementBox.height : y;
           }
-          return this.getAbsoluteY(y);
+          else if (this.positioning === 'static') {
+              return y;
+          }
+          else if (this.positioning === 'fixed') {
+              return (this.box.height - this.elementBox.height) / 2;
+          }
+          return this.getRelativeY(y);
       };
-      Position.prototype.getAbsoluteX = function (x) {
+      Position.prototype.getRelativeX = function (x) {
           return this.elementBox.width + x > this.box.width ? this.box.width - x : x;
       };
-      Position.prototype.getAbsoluteY = function (y) {
+      Position.prototype.getRelativeY = function (y) {
           return this.elementBox.height + y > this.box.height ? this.box.height - y : y;
       };
       return Position;
@@ -108,35 +152,6 @@
       return Box2D;
   }());
 
-  var Device = {
-      isMobile: false,
-      isDesktop: false
-  };
-  Object.defineProperty(Device, 'isMobile', {
-      get: function () {
-          var mobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|Windows Phone/i;
-          if (navigator.userAgent.match(mobile)) {
-              return true;
-          }
-          return false;
-      },
-      enumerable: true,
-      configurable: true
-  });
-  Object.defineProperty(Device, 'isDesktop', {
-      get: function () { return !Device.isMobile; },
-      enumerable: true,
-      configurable: true
-  });
-
-  var Path = /** @class */ (function () {
-      function Path(x, y) {
-          this.x = x;
-          this.y = y;
-      }
-      return Path;
-  }());
-
   var VALIDATE = 'validate';
   var FAILED = 'failed';
   var CLICK_AWAY = 'clickaway';
@@ -147,19 +162,31 @@
       CTXMENU: 'contextmenu'
   };
   var EVENT_OPT = true;
+  var NAME = 'ContextMenu';
+  // @ts-ignore
+  var DUMP = window[NAME];
   var ContextMenu = /** @class */ (function () {
       function ContextMenu(src, dest, windowSize) {
           this.src = src;
           this.dest = dest;
           this.timeout = 800;
-          this.positioning = 'absolute';
+          this.positioning = 'relative';
           this.handlers = {};
           var box = new Box2D(0, 0, windowSize.width, windowSize.height);
           var box2 = new Box2D(0, 0, this.dest.offsetWidth, this.dest.offsetHeight);
           this.ps = new Position(box, box2);
       }
+      // tslint:disable-next-line:member-ordering
+      ContextMenu.namespace = function (name) {
+          // @ts-ignore
+          window[name] = window[name] || {};
+          // @ts-ignore
+          window[name][NAME] = ContextMenu;
+          // @ts-ignore
+          window[NAME] = DUMP;
+      };
       ContextMenu.prototype.setup = function (setup) {
-          if (setup === void 0) { setup = { timeout: 800, position: 'absolute' }; }
+          if (setup === void 0) { setup = { timeout: 800, position: 'relative' }; }
           this.timeout = setup.timeout;
           this.positioning = setup.position;
           this.ps.type = this.positioning;
@@ -174,8 +201,8 @@
               touchMoved = true;
           };
           var end = function () {
-              _this.src.removeEventListener(Events.TOUCHMOVE, move, true);
-              _this.src.removeEventListener(Events.TOUCHEND, end, true);
+              _this.src.removeEventListener(Events.TOUCHMOVE, move, EVENT_OPT);
+              _this.src.removeEventListener(Events.TOUCHEND, end, EVENT_OPT);
               clearTimeout(id);
           };
           var touchMoved = false;
@@ -187,10 +214,10 @@
               _this.src.addEventListener(Events.TOUCHMOVE, move, EVENT_OPT);
               _this.src.addEventListener(Events.TOUCHEND, end, EVENT_OPT);
               var signal = new Path(touchEvent.touches[0].clientX, touchEvent.touches[0].clientY);
-              var response = _this.ps.from(signal);
+              var coords = _this.ps.from(signal);
               id = setTimeout(function () {
                   if (!touchMoved && typeof _this.handlers[VALIDATE] === 'function') {
-                      _this.handlers[VALIDATE].call(_this, new Path(response.x, response.y), _this.ps.viewportExcess(signal));
+                      _this.handlers[VALIDATE].call(_this, coords, _this.ps.viewportExcess(signal));
                   }
                   else if (touchMoved && typeof _this.handlers[FAILED] === 'function') {
                       _this.handlers[FAILED].call(_this);
@@ -202,9 +229,9 @@
               ctxEvent.preventDefault();
               var signal = new Path(ctxEvent.clientX, ctxEvent.clientY);
               var response = _this.ps.from(signal);
-              _this.handlers[VALIDATE].call(_this, new Path(response.x, response.y), _this.ps.viewportExcess(signal));
+              _this.handlers[VALIDATE].call(_this, response, _this.ps.viewportExcess(signal));
           };
-          if (Device.isMobile || navigator.maxTouchPoints || navigator.msMaxTouchPoints) {
+          if (Device.isMobile && (navigator.maxTouchPoints || navigator.msMaxTouchPoints)) {
               this.src.addEventListener(Events.TOUCHSTART, this.start, EVENT_OPT);
           }
           else {

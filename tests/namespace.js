a87e1a1fcaab46c71938f0e27719586bb9a7e159
CalebPitan <36812893+CalebPitan@users.noreply.github.com>
2019-09-29 02:06:42 +0100

    restore global variable

--- tests/namespace.js
+++ tests/namespace.js
@@ -1,5 +1,11 @@
 describe('Namespace', function() {
   var assert = chai.assert
+  
+  afterEach(function() {
+    window.ContextMenu = window.CalebPitan.ContextMenu
+    window.CalebPitan = void 0
+  })
+
   it('should reinstate `Dump` and get `Dumped` else where when namespaced', function() {
     assert.exists(window.ContextMenu, 'ContextMenu was never defined')
     ContextMenu.namespace('CalebPitan')

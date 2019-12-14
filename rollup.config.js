7cab5495e5f6546806bd7d379329eb68ea37b277
CalebPitan <36812893+CalebPitan@users.noreply.github.com>
2019-12-14 17:02:09 +0100

    Add banner to build
    Build script should add banner to compiled source files.
    
    The banner contains some details about the code.

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,21 @@
 import typescript from 'rollup-plugin-typescript3'
 import path from 'path'
 
+const pkg = require('./package.json')
+const date = new Date()
+const banner = `/*! Contextmenu v${pkg.version}
+* Copyright (c) ${date.getFullYear()} ${pkg.author}
+* Licensed under the ${pkg.license} License
+* https://github.com/calebpitan/contextmenu/blob/master/LICENSE
+* Build Date: ${date.toISOString()}
+*/`
 export default {
   input: path.resolve(__dirname, 'src/index.ts'),
   output: [{
     name: 'ContextMenu',
     file: path.resolve(__dirname, `dist/contextmenu${process.env.ESM === 'true' ? '.esm' : ''}.js`),
-    format: process.env.ESM === 'true' ? 'esm' : 'umd'
+    format: process.env.ESM === 'true' ? 'esm' : 'umd',
+    banner
   }],
   plugins: [
     typescript()
